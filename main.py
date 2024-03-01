59dc7332c010d28436f4cf3bc6798267a9e7da9d
PRAJWAL PURUSHOTHAM VAREKAR <rumor.vocal-0a@icloud.com>
2024-03-01 09:47:45 +0530

    if-else functon added

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 a = int(input("Enter the first number"))
 b = int(input("Enter the second number"))
+fun = input("Enter the fun")
 
-print("the additon of two number is ",a+b)
-
-print("the additon of two number is ",a-b)
-
-print("the additon of two number is ",a*b)
-
-print("the additon of two number is ",a/b)
+if fun == "add":
+    print("the additon of two number is ",a+b)
+elif fun == "sub" :
+    print("the additon of two number is ",a-b)
+elif fun == "mul" :
+    print("the additon of two number is ",a*b)
+elif fun == "div" :
+    print("the additon of two number is ",a/b)
+else :
+    print("Invaild fun")
