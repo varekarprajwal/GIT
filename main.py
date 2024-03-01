ac2c55792ad11a78d4791b3f60ba244e2f458d3c
PRAJWAL PURUSHOTHAM VAREKAR <rumor.vocal-0a@icloud.com>
2024-03-01 09:53:55 +0530

    bug fixing

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 a = int(input("Enter the first number"))
 b = int(input("Enter the second number"))
 
-print("the additon of two number is ",a+b)
+print("the addition of two number is ",a+b)
 
-print("the additon of two number is ",a-b)
+print("the subtraction of two number is ",a-b)
 
-print("the additon of two number is ",a*b)
+print("the multiplication of two number is ",a*b)
 
-print("the additon of two number is ",a/b)
+print("the division of two number is ",a/b)
