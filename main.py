4761d79e33edd0bdee716c5cf3374f8b7bfbf01e
PRAJWAL PURUSHOTHAM VAREKAR <rumor.vocal-0a@icloud.com>
2024-03-01 09:49:58 +0530

    bug fixing

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ b = int(input("Enter the second number"))
 fun = input("Enter the fun")
 
 if fun == "add":
-    print("the additon of two number is ",a+b)
+    print("the addition of two number is ",a+b)
 elif fun == "sub" :
-    print("the additon of two number is ",a-b)
+    print("the subtraction of two number is ",a-b)
 elif fun == "mul" :
-    print("the additon of two number is ",a*b)
+    print("the multiplication of two number is ",a*b)
 elif fun == "div" :
-    print("the additon of two number is ",a/b)
+    print("the division  of two number is ",a/b)
 else :
-    print("Invaild fun")
+    print("Invalid fun")
