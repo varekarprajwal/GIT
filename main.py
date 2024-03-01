fc8338b30cecc25fcc98bed0e79142c794ea6d32
PRAJWAL PURUSHOTHAM VAREKAR <rumor.vocal-0a@icloud.com>
2024-03-01 10:55:32 +0530

    funnction added

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+def Caculation( a,b,fun):
+    if fun == "add":
+        print("the addition of two number is ",a+b)
+    elif fun == "sub" :
+        print("the subtraction of two number is ",a-b)
+    elif fun == "mul" :
+        print("the multiplication of two number is ",a*b)
+    elif fun == "div" :
+        print("the division  of two number is ",a/b)
+    else :
+        print("Invalid fun")
+
+
+
 a = int(input("Enter the first number"))
 b = int(input("Enter the second number"))
 fun = input("Enter the fun")
-#hello worldss
-if fun == "add":
-    print("the addition of two number is ",a+b)
-elif fun == "sub" :
-    print("the subtraction of two number is ",a-b)
-elif fun == "mul" :
-    print("the multiplication of two number is ",a*b)
-elif fun == "div" :
-    print("the division  of two number is ",a/b)
-else :
-    print("Invalid fun")
+
+Caculation(a,b,fun)
